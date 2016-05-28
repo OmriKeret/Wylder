06e7f5296af9a715761511f7b56d92a0cb803a8e
OmriKeret <omri.keret@gmail.com>
2016-05-29 01:04:27 +0300

    added control plugin

---
+++ Assets/Scripts/Character/playerDeathLogic.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerDeathLogic : MonoBehaviour {
+
+	public Vector3 respawnTo;
+	private int currentCheckpointNum = -1;
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag.Equals ("checkpoint")) {
+			int checkpointNum = other.GetComponent<checkpoint> ().checkpointNum;
+			if (checkpointNum > currentCheckpointNum) {
+				currentCheckpointNum = checkpointNum;
+				respawnTo = other.transform.position;
+			}
+		}
+	} 
+
+	public void die() {
+		AutoFade.FadeOut (Color.black, moveCharPosition);		
+	}
+
+	private void moveCharPosition() {
+		this.transform.position = respawnTo;
+		AutoFade.FadeIn (null);
+	}
+}
---
+++ Assets/Scripts/GeneralUtils/checkpoint.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class checkpoint : MonoBehaviour {
+	public int checkpointNum;
+}
