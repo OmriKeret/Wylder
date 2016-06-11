c91d21d14f94d4a43b18fb4b1c1222acb36e6e3b
Or Sivan <orsivan22@gmail.com>
2016-06-11 13:01:22 +0300

    Adding Collision interface

---
+++ Assets/Scripts/GeneralUtils/ICharCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface ICharCollider {
+
+    eCharState GetState();
+    void ActiveHitAnimation();
+    void ActiveDeathAnimation();
+    void ActiveHitSound();
+    void ActiveDeathSound();
+    bool Hit(int dmg); //(return true if the hitted char is dead after the hit)
+    void ActiveCounterAnimation();// (we need to think how to synchronize both on the animation)
+
+}
