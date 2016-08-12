34473f2532fe7fa80a80ab3134bdf84ccf4b67d9
OmriKeret <omri.keret@gmail.com>
2016-08-13 00:54:05 +0300

    added animationstion

---
+++ Assets/playMovie.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playMovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Animator> ().Play ("start_movie");
+	}
+
+	public void endMovie() {
+		AutoFade.FadeOut(Color.black, moveToFirstLevel);
+	}
+
+	public void moveToFirstLevel() {
+		Application.LoadLevel("level1");	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
