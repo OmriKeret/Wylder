26420c9e2da225b6e1f17cc659ccd281ef307282
Or Sivan <orsivan22@gmail.com>
2016-08-20 23:59:16 +0300

    Added enemy weapon

---
+++ Assets/ZombieWeapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieWeapon : MonoBehaviour
+{
+
+    public CollisionFacade collisionFacade;
+    private GameObject root;
+    // Use this for initialization
+    void Start()
+    {
+        root = transform.root.gameObject;
+        collisionFacade = root.GetComponent<Zombie>().GetCollisionFacade();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+        collisionFacade.Collision(root, col.gameObject);
+    }
+
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        //collisionFacade.Collision(this.gameObject, col.gameObject);
+    }
+
+
+}
