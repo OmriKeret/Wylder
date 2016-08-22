11469217a24b2652ae719e654807e001e9eebe15
OmriKeret <omri.keret@gmail.com>
2016-08-22 22:47:36 +0300

    mainmenu done

---
+++ Assets/menuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+
+	public void quitGame() {
+		Application.Quit();
+	}	
+
+	public void startGame() {
+		AutoFade.FadeOut(Color.black, moveToFirstLevel);
+	}
+
+	public void moveToFirstLevel() {
+		Application.LoadLevel(1);	
+	}
+}
