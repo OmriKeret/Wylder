995a21b08dd85d233e372ae84325a13a9a192018
Or Sivan <orsivan22@gmail.com>
2016-08-23 05:11:34 +0300

    Enemy Sound

---
+++ Assets/EnemyAnimationHolder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Reflection;
+using System.Collections;
+
+public class EnemyAnimationHolder : MonoBehaviour {
+
+    public string Idle;
+    public string Run;
+    public string Jump;
+    public string Air;
+    public string Attack1, Attack2, Attack3;
+    public string Counter;
+    public string RecieveHit;
+    public string Death;
+
+    // Use this for initialization
+    void Start () {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Assets/ZombieSoundHolder.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieSoundHolder : MonoBehaviour {
+
+    public AudioClip Death;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
